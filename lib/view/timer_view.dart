1c6c7a7d2fd7196796de64af63275c3e1b8599db
JasonKK <hyeon7427@gmail.com>
2022-04-20 15:02:22 +0900

    Made TimerPage

--- lib/view/timer_view.dart
+++ lib/view/timer_view.dart
@@ -1,7 +1,7 @@
 /* this is timer_model.dart */
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:bom_front/view/main_view.dart';
-import 'package:simple_timer/simple_timer.dart';
 
 class TimerPage extends StatefulWidget {
   const TimerPage({Key? key, required this.title}) : super(key: key);
@@ -12,70 +12,14 @@ class TimerPage extends StatefulWidget {
   State<TimerPage> createState() => _TimerPageState();
 }
 
-class _TimerPageState extends State<TimerPage>
-    with SingleTickerProviderStateMixin {
-  String _buttonState = 'OFF';
-  var _color = Colors.blue;
-  late TimerController _timerController;
-  TimerStyle _timerStyle = TimerStyle.ring;
-  TimerProgressIndicatorDirection _progressIndicatorDirection =
-      TimerProgressIndicatorDirection.clockwise;
-  TimerProgressTextCountDirection _textCountDirection =
-      TimerProgressTextCountDirection.count_up;
+class _TimerPageState extends State<TimerPage> {
+  String timeString = "00:00:00";
+  Stopwatch stopwatch = Stopwatch();
+  late Timer timer;
 
   @override
-  void initState() {
-    //intialize timercontroller
-    _timerController = TimerController(this);
-    super.initState();
-  }
-
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white60,
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          textAlign: TextAlign.center,
-        ),
-        centerTitle: true,
-      ),
-    );
-  }
+    // TODO: implement build
+    throw UnimplementedError();
+  }ㄴ
 }
-
-//
-// class TimerPage extends StatelessWidget {
-//   final Plan plan;
-//
-//   TimerController _timerController;
-//
-//   TimerPage({required Plan this.plan});
-//
-//
-//   @override
-//   void initState() {
-//     _timerController = TimerController(this);
-//     super.initState();
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text("This is Timer page"),
-//         ),
-//         body: ElevatedButton(
-//           child: Text('id : ${plan.planId}'),
-//           onPressed: () {
-//             Navigator.pop(context);
-//           },
-//         ),
-//       Container(
-//         child: SimpleTimer(
-//           controller : _timerController,
-//           duration : Duration(seconds: 10),
-//         )
-//       )
-//     );
-//   }
-// }
