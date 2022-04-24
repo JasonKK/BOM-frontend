dadc071ea7c50759518a52277a75496be8f19944
JasonKK <hyeon7427@gmail.com>
2022-04-25 04:24:33 +0900

    origin design of timer app

--- lib/view/timer_view.dart
+++ lib/view/timer_view.dart
@@ -20,70 +20,284 @@ class TimerPage extends StatefulWidget {
 }
 
 class _TimerPageState extends State<TimerPage> {
+  var _icon = Icons.play_arrow;
+  var _Color = Colors.amber;
+
+  late Timer _timer; //타이머
+  var _time = 0; //실제 늘어난 시간
+  var _isRunning = false; // 시작/정지의 상태값
+
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "국어모의고사 1회",
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ), // 나중에 과목api 받아오기
-          centerTitle: true,
-          elevation: 0.0,
-          leading: IconButton(
-            //뒤로가기 버튼
-            icon: const Icon(Icons.arrow_back),
-            tooltip: "Next page",
-            onPressed: () {
-              Navigator.pop(context); // 뒤로가기
-            },
-          ),
+      appBar: AppBar(
+        title: Text(
+          "국어모의고사 1회",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ), // 나중에 과목api 받아오기
+        centerTitle: true,
+        elevation: 0.0,
+        leading: IconButton(
+          //뒤로가기 버튼
+          icon: const Icon(Icons.arrow_back),
+          tooltip: "Next page",
+          onPressed: () {
+            Navigator.pop(context); // 뒤로가기
+          },
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+      ),
+      body: _buildbody(),
+    );
+  }
+
+  Widget _buildbody() {
+    var min = '${_time ~/ 6000}'.padLeft(2, '0');
+    var sec = '${_time ~/ 100}'.padLeft(2, '0');
+    var hundredth = '${_time % 100}'.padLeft(2, '0');
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 50),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                ),
+                Container(
+                  width: 360,
+                  height: 360,
+                  // color: Colors.grey,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.black38,
+                      width: 5,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(10, 10),
+                          color: Colors.black38,
+                          blurRadius: 15),
+                      BoxShadow(
+                          offset: Offset(-10, -10),
+                          color: Colors.white.withOpacity(0.5),
+                          blurRadius: 15),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              child: Image.asset(
-                'lib/image/dog.png',
-                width: 80,
-                height: 80,
+            Positioned(
+              top: -20,
+              right: 110,
+              child: Container(
+                child: Image.asset(
+                  'lib/image/dog.png',
+                  width: 80,
+                  height: 80,
+                ),
               ),
             ),
-            Center(
+            Positioned(
+              top: 150,
+              right: 30,
+              child: Text(
+                "$min:$sec:$hundredth",
+                style: TextStyle(fontSize: 60, color: Colors.grey.shade900),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              right: 70,
               child: Container(
-                width: 360,
-                height: 360,
-                // color: Colors.grey,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(10, 10),
-                        color: Colors.black38,
-                        blurRadius: 15),
-                    BoxShadow(
-                        offset: Offset(-10, -10),
-                        color: Colors.white.withOpacity(0.5),
-                        blurRadius: 15),
-                  ],
+                child: Text(
+                  "오늘은: 00:00:00",
+                  style: TextStyle(fontSize: 20, color: Colors.grey.shade900),
                 ),
-
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "00:00:00",
-                      style:
-                          TextStyle(fontSize: 80, color: Colors.grey.shade900),
-                    )
-                  ],
-                ),
+              ),
+            ),
+            Positioned(
+              top: 130,
+              right: 80,
+              child: TextButton(
+                onPressed: () => setState(() {
+                  _clickButton();
+                }),
+                child: _isRunning
+                    ? Icon(
+                        Icons.pause,
+                        size: 100,
+                      )
+                    : Icon(
+                        Icons.play_arrow,
+                        size: 100,
+                      ),
               ),
             ),
           ],
-        ));
+        ),
+      ),
+    );
+  }
+
+  void _clickButton() {
+    _isRunning = !_isRunning;
+
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer.cancel();
   }
 }
+
+//     clipBehavior: Clip.none, //안짤리게 해주는거
+//     alignment: AlignmentDirectional.topCenter,
+//     fit: StackFit.loose,
+//     children: [
+//     Container(
+//     width: 360,
+//     height: 360,
+//     // color: Colors.grey,
+//     decoration: BoxDecoration(
+//     color: Colors.grey.shade200,
+//     shape: BoxShape.circle,
+//     border: Border.all(
+//     color: Colors.black38,
+//     width: 5,
+//     ),
+//     boxShadow: [
+//     BoxShadow(
+//     offset: Offset(10, 10),
+//     color: Colors.black38,
+//     blurRadius: 15),
+//     BoxShadow(
+//     offset: Offset(-10, -10),
+//     color: Colors.white.withOpacity(0.5),
+//     blurRadius: 15),
+//     ],
+//     ),
+//     ),
+//     Positioned(
+//     top: -40,
+//     height: 100,
+//     child: Container(
+//     child: Image.asset(
+//     'lib/image/dog.png',
+//     width: 80,
+//     height: 80,
+//     ),
+//     ),
+//     ),
+//     Positioned(
+//     top: 90,
+//     child: Container(
+//     child: Text(
+//     "오늘은: 00:00:00",
+//     style: TextStyle(
+//     fontSize: 20, color: Colors.grey.shade900),
+//     ),
+//     ),
+//     ),
+//     Positioned(
+//     top: 130,
+//     child: Container(
+//     child: Text(
+//     "00:00:00",
+//     style: TextStyle(
+//     fontSize: 80, color: Colors.grey.shade900),
+//     ),
+//     ),
+//     ),
+//
+//     Positioned(
+//     top: 100,
+//     child: FlatButton(
+//     onPressed: () {},
+//     child: Container(
+//     child: Icon(
+//     Icons.play_arrow,
+//     color: Colors.black38,
+//     size: 180,
+//     ),
+//     ),
+//     ),
+//     ),
+//     Positioned(
+//     top: 100,
+//     child: FlatButton(
+//     onPressed: () {},
+//     child: Container(
+//     child: Icon(
+//     Icons.stop,
+//     color: Colors.black38,
+//     size: 180,
+//     ),
+//     ),
+//     ),
+//     ),
+//     ),
+//     ),
+//     ),
+//   };
+// }
+
+//
+//   void _clickButton() {
+//     _isRunning = !_isRunning;
+//
+//     if (_isRunning) {
+//       _start();
+//     }
+//     else {
+//       _pause();
+//     }
+//   }
+//
+//   void _start(){
+//     _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+//       setState((){
+//         _time++;
+//       });
+//     });
+//
+//   }
+//
+//   void _pause() {
+//     _timer.cancel();
+//   }
+//
+//   void _reset() {
+//     setState((){
+//       _isRunnning = false;
+//       _timer.cancel();
+//       _time = 0;
+//     });
+//   }
+//
+// }
+//
+//
+//
