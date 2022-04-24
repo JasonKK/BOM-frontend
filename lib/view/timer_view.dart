434394f794a109d86d242b6932129c069d595f89
JasonKK <hyeon7427@gmail.com>
2022-04-24 12:39:59 +0900

    timer image(character) inserted

--- lib/view/timer_view.dart
+++ lib/view/timer_view.dart
@@ -1,6 +1,7 @@
 /* this is timer_model.dart */
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:math';
 // import 'package:http/http.dart' as http;
 import 'package:bom_front/view/main_view.dart';
 
@@ -22,7 +23,7 @@ class _TimerPageState extends State<TimerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           title: Text(
             "국어모의고사 1회",
             style: TextStyle(color: Colors.white, fontSize: 20),
@@ -36,7 +37,53 @@ class _TimerPageState extends State<TimerPage> {
             onPressed: () {
               Navigator.pop(context); // 뒤로가기
             },
-          )),
-    );
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+            ),
+            Container(
+              child: Image.asset(
+                'lib/image/dog.png',
+                width: 80,
+                height: 80,
+              ),
+            ),
+            Center(
+              child: Container(
+                width: 360,
+                height: 360,
+                // color: Colors.grey,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(10, 10),
+                        color: Colors.black38,
+                        blurRadius: 15),
+                    BoxShadow(
+                        offset: Offset(-10, -10),
+                        color: Colors.white.withOpacity(0.5),
+                        blurRadius: 15),
+                  ],
+                ),
+
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "00:00:00",
+                      style:
+                          TextStyle(fontSize: 80, color: Colors.grey.shade900),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
