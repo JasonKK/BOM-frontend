16a4b9fb3f338261562714d3bc47d8675749d826
JasonKK <hyeon7427@gmail.com>
2022-04-27 14:04:27 +0900

    pull planner

--- lib/model/timer_model.dart
+++ lib/model/timer_model.dart
@@ -1,1 +1,27 @@
 /* this is timer_model.dart */
+
+// class Timer {
+//   int? planId;
+//   int time = 0;
+//
+//   Timer(this.planId, this.time);
+//
+//   Timer.fromJson(Map<String, dynamic> timerMap) {
+//     planId = timerMap['planId'] ?? 0;
+//     time = timerMap['time'] ?? 0;
+//   }
+//
+//   Map<String, dynamic> toJson() {
+//     return {'planId': planId, 'time': time};
+//   }
+// }
+
+import 'package:http/http.dart' as http;
+
+//http://ec2-3-39-177-232.ap-northeast-2.compute.amazonaws.com/
+class HttpHelper {
+
+  final String domain = 'http://ec2-3-39-177-232.ap-northeast-2.compute.amazonaws.com';
+  final String path = ''
+
+}
