ae74b934a90d42ce21506e907fd849504fc80fb1
JasonKK <hyeon7427@gmail.com>
2022-04-29 00:36:29 +0900

    set up timer_repository.dart

--- lib/repository/timer_repository.dart
+++ lib/repository/timer_repository.dart
@@ -9,7 +9,7 @@ class TimerRepository {
   Future<Todo> getTimerData() async {
     print("this is for get category color, plan name, time");
     final http.Response response =
-        await http.get(Uri.parse(urlApi + '/:planId=1/data'));
+        await http.get(Uri.parse(urlApi + '/plan/1/data'));
 
     if (response.statusCode == 200) {
       return Todo.fromJson(jsonDecode(response.body));
@@ -22,24 +22,8 @@ class TimerRepository {
   Future<Todo> postStarData(int num) async {
     print("this is count seconds and send how many stars user get");
 
-    final http.Response response = await http.post(
-        Uri.parse(urlApi + '/plan/star'),
-        body: <String, int>{"star": num},
-        headers: <String, String>{'Content-type': 'application/json'});
-
-    if (response.statusCode == 200) {
-      return Todo.fromJson(jsonDecode(response.body));
-    } else {
-      print("ERROR by this status : ${response.statusCode}");
-      throw Exception("Failed to load timerdata");
-    }
-  }
-
-  Future<Todo> getStarData() async {
-    // 문제는 todo에 별이 없다는거!
-    print("this is for get star num");
-    final http.Response response = await http
-        .get(Uri.parse(urlApi + '/plan/star?date=2022-04-27&userId=1'));
+    final http.Response response =
+        await http.post(Uri.parse(urlApi + '/plan/star'));
 
     if (response.statusCode == 200) {
       return Todo.fromJson(jsonDecode(response.body));
