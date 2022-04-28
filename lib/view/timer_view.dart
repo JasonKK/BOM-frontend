4408350e4093794feb646630210d1177b8385975
JasonKK <hyeon7427@gmail.com>
2022-04-28 20:06:07 +0900

    add timer_view.dart

--- lib/view/timer_view.dart
+++ lib/view/timer_view.dart
@@ -1,7 +1,17 @@
 /* this is timer_model.dart */
+import 'package:bom_front/model/todo.dart';
+import 'package:bom_front/repository/timer_repository.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:bom_front/view/main_view.dart';
+import 'package:bom_front/view/components/timer_appbar.dart';
+
+class TimerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'StopWatch', home: TimerPage());
+  }
+}
 
 class TimerPage extends StatefulWidget {
   @override
@@ -9,120 +19,158 @@ class TimerPage extends StatefulWidget {
 }
 
 class _TimerPageState extends State<TimerPage> {
-  String timeString = "00:00:00";
-  Stopwatch stopwatch = Stopwatch();
-  late Timer timer;
+  late Timer _timer; //타이머
+  var _time = 0; //실제 늘어난 시간
+  var _isRunning = false; // 시작/정지의 상태
 
-  void start() {
-    stopwatch.start();
-    timer = Timer.periodic(Duration(milliseconds: 1), update);
+  Todo todo = new Todo();
+
+  void initState() {
+    TimerRepository().getTimerData();
+    super.initState();
+    updateData();
+    //해당 일정의 시간, 카테고리 색,일정 내용 가져와야함
   }
 
-  void update(Timer t) {
-    if (stopwatch.isRunning) {
-      setState(() {
-        timeString =
-            (stopwatch.elapsed.inMinutes % 60).toString().padLeft(2, "0") +
-                ":" +
-                (stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, "0") +
-                ":" +
-                (stopwatch.elapsed.inMilliseconds % 1000 / 10)
-                    .clamp(0, 99)
-                    .toStringAsFixed(0)
-                    .padLeft(2, "0");
-      });
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void updateData() {
+    _time = todo.time!;
+  }
+
+  void _clickButton() {
+    _isRunning = !_isRunning;
+
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
     }
   }
 
-  void stop() {
-    setState(() {
-      timer.cancel();
-      stopwatch.stop();
+  void _start() {
+    int count = 0;
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+        count++;
+        if (count == 30) {
+          count = 0;
+          TimerRepository()
+              .postStarData(1); // 별개수 받아와야하는데 그거에 대해서 논의의          //별보내기
+        }
+      });
     });
   }
 
-  void reset() {
-    timer.cancel();
-    stopwatch.reset();
-    setState(() {
-      timeString = "00:00:00";
-    });
-    stopwatch.stop();
+  void _pause() {
+    _timer.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade900,
-      body: Column(children: <Widget>[
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 100),
-            child: Text("Stopwatch",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey,
-                ))),
-        Expanded(
-          child: Container(
-              width: 280,
-              height: 280,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                      offset: Offset(10, 10),
-                      color: Colors.black38,
-                      blurRadius: 15),
-                  BoxShadow(
-                      offset: Offset(-10, -10),
-                      color: Colors.white.withOpacity(0.55),
-                      blurRadius: 15),
-                ],
-              ),
-              child: Column(
+      appBar: TimerAppBar(),
+      body: _buildbody(),
+    );
+  }
+
+  Widget _buildbody() {
+    var min = '${_time ~/ 6000}'.padLeft(2, '0');
+    var sec = '${_time ~/ 100}'.padLeft(2, '0');
+    var hundredth = '${_time % 100}'.padLeft(2, '0');
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 50),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            Column(
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "00:00:00",
-                      style:
-                          TextStyle(fontSize: 40, color: Colors.grey.shade900),
-                    )
-                  ])),
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                ),
+                Container(
+                  width: 360,
+                  height: 360,
+                  // color: Colors.grey,
+                  decoration: BoxDecoration(
+                    // color: '$color',
+                    color: Colors.grey.shade200,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.black38,
+                      width: 5,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(10, 10),
+                          color: Colors.black38,
+                          blurRadius: 15),
+                      BoxShadow(
+                          offset: Offset(-10, -10),
+                          color: Colors.white.withOpacity(0.5),
+                          blurRadius: 15),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              top: -20,
+              right: 110,
+              child: Container(
+                child: Image.asset(
+                  'lib/image/dog.png',
+                  width: 80,
+                  height: 80,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 150,
+              right: 30,
+              child: Text(
+                "$min:$sec:$hundredth",
+                style: TextStyle(fontSize: 60, color: Colors.grey.shade900),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              right: 70,
+              child: Container(
+                child: Text(
+                  "오늘은: 00:00:00",
+                  style: TextStyle(fontSize: 20, color: Colors.grey.shade900),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 130,
+              right: 80,
+              child: TextButton(
+                onPressed: () => setState(() {
+                  _clickButton();
+                }),
+                child: _isRunning
+                    ? Icon(
+                        Icons.pause,
+                        size: 100,
+                      )
+                    : Icon(
+                        Icons.play_arrow,
+                        size: 100,
+                      ),
+              ),
+            ),
+          ],
         ),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 60),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  FlatButton(
-                      onPressed: () {},
-                      child: Container(
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          shape: BoxShape.circle,
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(10, 10),
-                                color: Colors.black38,
-                                blurRadius: 15),
-                            BoxShadow(
-                                offset: Offset(-10, -10),
-                                color: Colors.white.withOpacity(0.85),
-                                blurRadius: 15)
-                          ],
-                        ),
-                        child: Icon(
-                            stopwatch.isRunning
-                                ? Icons.pause
-                                : Icons.play_arrow,
-                            size: 60),
-                      )),
-                ]))
-      ]),
+      ),
     );
   }
 }
