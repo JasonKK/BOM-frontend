47612b9cdc9dce640d87dcbbb3c8bdfc2e3e7562
JasonKK <hyeon7427@gmail.com>
2022-05-01 00:53:23 +0900

    updated statistic_model.dart and statistic_repo.dart

--- lib/repository/statistic_repo.dart
+++ lib/repository/statistic_repo.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:bom_front/model/statistic_model.dart';
+
 import '../model/todo.dart';
 import 'package:http/http.dart' as http;
 
@@ -6,16 +8,127 @@ class StatisticRepository {
   static const urlApi =
       'http://ec2-3-39-177-232.ap-northeast-2.compute.amazonaws.com';
 
-  Future<Todo> getChartData() async {
-    print("this is for get time per date");
+  /*하루 시간 가져오는 코드 */
+  Future<Statistic> getDailyTime() async {
+    print("this is for get daily time data");
     final http.Response response = await http
         .get(Uri.parse(urlApi + '/plan/total?date=2022-04-26&userId=1'));
 
     if (response.statusCode == 200) {
-      return Todo.fromJson(jsonDecode(response.body));
+      Map<String, dynamic> body = json.decode(response.body);
+      if (body['averageTime'] == null) {
+        print('Error! because averageTime is empty');
+      }
+      return body['averageTime'];
     } else {
       print("ERROR by this status : ${response.statusCode}");
       throw Exception("Failed to load timerdata");
+    }
+  }
+
+  /*한주의 별개수 가져오는 코드 */
+  Future<Statistic> getWeekStar() async {
+    print("this is for get week star data");
+    final http.Response response = await http
+        .get(Uri.parse(urlApi + '/plan/week/star?date=2022-04-27&userId=2'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = json.decode(response.body);
+      if (body['stars'] == null) {
+        print('Error! because star is empty');
+      }
+      return body['stars'];
+    } else {
+      print("ERROR by this status : ${response.statusCode}");
+      throw Exception("Failed to load star data");
+    }
+  }
+
+  /*한달의 별개수 가져오는 코드 */
+  Future<Statistic> getMonthstar() async {
+    print("this is for get month star data");
+    final http.Response response = await http
+        .get(Uri.parse(urlApi + '/plan/month/star?date=2022-04-27&userId=2'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = json.decode(response.body);
+      if (body['stars'] == null) {
+        print('Error! because plan is empty');
+      }
+      return body['stars'];
+    } else {
+      print("ERROR by this status : ${response.statusCode}");
+      throw Exception("Failed to load star data");
+    }
+  }
+
+/*한주의 시간 가져오는 코드 */
+  Future<Statistic> getWeekTime() async {
+    print("this is for get week time data");
+    final http.Response response = await http
+        .get(Uri.parse(urlApi + '/plan/week/average?date=2022-04-27&userId=2'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = json.decode(response.body);
+      if (body['averageTime'] == null) {
+        print('Error! because averageTime is empty');
+      }
+      return body['averageTime'];
+    } else {
+      print("ERROR by this status : ${response.statusCode}");
+      throw Exception("Failed to load time data");
+    }
+  }
+
+/*한달의간시간 가져오는 코드 */
+  Future<Todo> getMonthTime() async {
+    print("tthis is for get month time data");
+    final http.Response response = await http.get(
+        Uri.parse(urlApi + '/plan/month/average?date=2022-04-27&userId=2'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = json.decode(response.body);
+      if (body['averageTime'] == null) {
+        print('Error! because averageTime is empty');
+      }
+      return body['averageTime'];
+    } else {
+      print("ERROR by this status : ${response.statusCode}");
+      throw Exception("Failed to load time data");
+    }
+  }
+
+  Future<Statistic> getWeekTimeChart() async {
+    print("this is for get week time ");
+    final http.Response response = await http
+        .get(Uri.parse(urlApi + '/plan/week/total?date=2022-04-27&userId=2'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = json.decode(response.body);
+      if (body['averageTime'] == null) {
+        print('Error! because averageTime is empty');
+      }
+      return body['averageTime'];
+    } else {
+      print("ERROR by this status : ${response.statusCode}");
+      throw Exception("Failed to load time data");
+    }
+  }
+
+  Future<Statistic> getMonthTimChart() async {
+    print("this is for get month time ");
+    final http.Response response = await http
+        .get(Uri.parse(urlApi + '/plan/month/total?date=2022-04-27&userId=2'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = json.decode(response.body);
+      if (body['averageTime'] == null) {
+        print('Error! because averageTime is empty');
+      }
+      return body['averageTime'];
+    } else {
+      print("ERROR by this status : ${response.statusCode}");
+      throw Exception("Failed to load time data");
     }
   }
 }
