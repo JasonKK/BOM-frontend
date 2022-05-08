cd116084110cdd85d5d2f12d93c989b1d8821bb5
JasonKK <hyeon7427@gmail.com>
2022-05-09 03:31:03 +0900

    Edited statistic_repo.dart

--- lib/repository/statistic_repo.dart
+++ lib/repository/statistic_repo.dart
@@ -10,29 +10,11 @@ class StatisticRepository {
   static const urlApi =
       'http://ec2-3-39-10-54.ap-northeast-2.compute.amazonaws.com';
 
-  /*하루 시간 가져오는 코드 */
-  Future<Statistic> getDailyTime() async {
-    print("this is for get daily time data");
-    final http.Response response = await http
-        .get(Uri.parse(urlApi + '/plan/total?date=${getToday()}&userId=1'));
-
-    if (response.statusCode == 200) {
-      Map<String, dynamic> body = json.decode(response.body);
-      if (body['averageTime'] == null) {
-        print('Error! because averageTime is empty');
-      }
-      return body['averageTime'];
-    } else {
-      print("ERROR by this status : ${response.statusCode}");
-      throw Exception("Failed to load timerdata");
-    }
-  }
-
   /*한주의 별개수 가져오는 코드 */
-  Future<Statistic> getWeekStar() async {
+  Future<int> getWeekStar() async {
     print("this is for get week star data");
     final http.Response response = await http
-        .get(Uri.parse(urlApi + '/plan/week/star?date=${getToday()}&userId=2'));
+        .get(Uri.parse(urlApi + '/plan/week/star?date=${getToday()}&userId=1'));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> body = json.decode(response.body);
@@ -47,10 +29,10 @@ class StatisticRepository {
   }
 
   /*한달의 별개수 가져오는 코드 */
-  Future<Statistic> getMonthstar() async {
+  Future<int> getMonthstar() async {
     print("this is for get month star data");
     final http.Response response = await http.get(
-        Uri.parse(urlApi + '/plan/month/star?date=${getToday()}&userId=2'));
+        Uri.parse(urlApi + '/plan/month/star?date=${getToday()}&userId=1'));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> body = json.decode(response.body);
@@ -65,10 +47,10 @@ class StatisticRepository {
   }
 
 /*한주의 시간 가져오는 코드 */
-  Future<Statistic> getWeekTime() async {
+  Future<int> getWeekTime() async {
     print("this is for get week time data");
     final http.Response response = await http.get(
-        Uri.parse(urlApi + '/plan/week/average?date=${getToday()}&userId=2'));
+        Uri.parse(urlApi + '/plan/week/average?date=${getToday()}&userId=1'));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> body = json.decode(response.body);
@@ -83,10 +65,10 @@ class StatisticRepository {
   }
 
 /*한달의간시간 가져오는 코드 */
-  Future<Todo> getMonthTime() async {
+  Future<int> getMonthTime() async {
     print("tthis is for get month time data");
     final http.Response response = await http.get(
-        Uri.parse(urlApi + '/plan/month/average?date=${getToday()}&userId=2'));
+        Uri.parse(urlApi + '/plan/month/average?date=${getToday()}&userId=1'));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> body = json.decode(response.body);
@@ -100,34 +82,34 @@ class StatisticRepository {
     }
   }
 
-  Future<Statistic> getWeekTimeChart() async {
+  Future<List<int>> getWeekTimeChart() async {
     print("this is for get week time ");
     final http.Response response = await http.get(
-        Uri.parse(urlApi + '/plan/week/total?date=${getToday()}&userId=2'));
+        Uri.parse(urlApi + '/plan/week/total?date=${getToday()}&userId=1'));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> body = json.decode(response.body);
-      if (body['averageTime'] == null) {
+      if (body['timeSum'] == null) {
         print('Error! because averageTime is empty');
       }
-      return body['averageTime'];
+      return body['timeSum'];
     } else {
       print("ERROR by this status : ${response.statusCode}");
       throw Exception("Failed to load time data");
     }
   }
 
-  Future<Statistic> getMonthTimChart() async {
+  Future<List<int>> getMonthTimChart() async {
     print("this is for get month time ");
     final http.Response response = await http.get(
-        Uri.parse(urlApi + '/plan/month/total?date=${getToday()}&userId=2'));
+        Uri.parse(urlApi + '/plan/month/total?date=${getToday()}&userId=1'));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> body = json.decode(response.body);
-      if (body['averageTime'] == null) {
+      if (body['timeSum'] == null) {
         print('Error! because averageTime is empty');
       }
-      return body['averageTime'];
+      return body['timeSum'];
     } else {
       print("ERROR by this status : ${response.statusCode}");
       throw Exception("Failed to load time data");
